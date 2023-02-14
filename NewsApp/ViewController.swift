f66971ff6ad5934acab7c735848145c86d0dee28
Tag2075 <105149096+Tag2075@users.noreply.github.com>
2023-02-14 13:45:32 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by 1234 on 14.02.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
